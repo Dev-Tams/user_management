15bf7e9b89864945ff0b7cf4c33f7236160e0ddb
Dev-Tams <tammyalemu@gmail.com>
2025-07-15 01:46:20 +0100

    defining model

---
+++ model.go
@@ -0,0 +1,21 @@
+package main
+
+type User struct{
+	Name, Email string
+	ID int
+}
+
+type Admin struct{
+ 	User
+	Privileges [] string
+}
+
+type Editor struct{
+	User
+	CanEdit bool
+}
+
+type Viewer struct{
+	User
+	AccessLevel int
+}
